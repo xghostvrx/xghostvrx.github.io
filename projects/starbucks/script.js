5537018cd5b5de1c70e5543a2fe8d2459ef53426
George Testo <xghostvrx@gmail.com>
2024-04-09 14:01:50 -0700

    Add initial site code refactor (navbar)

--- projects/starbucks/script.js
+++ projects/starbucks/script.js
@@ -7,3 +7,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
