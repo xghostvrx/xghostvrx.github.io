448c31a00894e46256cf21a1d6469e87b64568e8
George Testo <xghostvrx@gmail.com>
2024-04-16 13:52:37 -0700

    Add page-4 (store + corporate) html and initial styling

--- projects/starbucks/script.js
+++ projects/starbucks/script.js
@@ -11,3 +11,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.side-nav').classList.toggle('active');
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
