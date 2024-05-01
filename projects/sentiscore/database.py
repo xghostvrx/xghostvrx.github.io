3498a225b72dd046d84746270a90d559d419d30d
George Testo <xghostvrx@gmail.com>
2024-05-01 13:28:29 -0700

    Add initialize_server() + check_database_exists() functions

--- projects/sentiscore/database.py
+++ projects/sentiscore/database.py
@@ -1,1 +1,22 @@
 # database.py
+import psycopg
+from os import getenv
+from dotenv import load_env
+
+load_env()
+
+def connect_to_database():
+    conn = psycopg.connect(
+        host=getenv('DB_HOSTNAME'),
+        user=getenv('DB_USERNAME'),
+        password=getenv('DB_PASSWORD')
+    )
+    cur = conn.cursor()
+    return conn, cur
+
+def check_database_exists():
+    conn, cur = connect_to_database()
+    cur.execute("SELECT sentiscore FROM pg_database;")
+    list_database = cur.fetchall()
+    conn.close()
+    return ('sentiscore',) in list_database
--- projects/sentiscore/server.py
+++ projects/sentiscore/server.py
@@ -1,5 +1,7 @@
 # server.py
+import sys
 from flask import Flask, render_template
+from database import check_database_exists
 
 app = Flask(__name__)
 
@@ -7,5 +9,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def initialize_server():
+    if check_database_exists():
+        app.run()
+    else:
+        print("Database 'sentiscore' does not exist.")
+        sys.exit()
+
 if __name__ == '__main__':
-    app.run()
+    initialize_server()
