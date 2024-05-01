4d4bf2f32b400fe888c1cbcf3ee05c2ee3fce5c5
George Testo <xghostvrx@gmail.com>
2024-05-01 13:49:31 -0700

    Revert "Rename check_database_exists() to check_table_exists() for improved clarity"
    This reverts commit f8bc766ca5fd35f9a3daee8992243cbe7f50988e.

--- projects/sentiscore/server.py
+++ projects/sentiscore/server.py
@@ -1,6 +1,6 @@
 # server.py
 from flask import Flask, render_template
-from database import check_table_exists, create_database
+from database import check_database_exists, create_database
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def index():
     return render_template('index.html')
 
 def initialize_server():
-    if check_table_exists():
+    if check_database_exists():
         app.run()
     else:
         print("Database 'sentiscore' does not exist.")
