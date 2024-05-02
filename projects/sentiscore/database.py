3cfa0d63efacbde1d825fcb283a12388484e6190
George Testo <xghostvrx@gmail.com>
2024-05-01 17:11:04 -0700

    Add check_database_exists() + check_table_exists() cases

--- projects/sentiscore/database.py
+++ projects/sentiscore/database.py
@@ -1,47 +1,43 @@
 # database.py
 from os import getenv
-from dotenv import load_env
 from psycopg import connect, OperationalError
 from sys import exit
-
-load_env()
 
 def connect_to_server():
     try:
         conn = connect(
-            host=getenv('DB_HOSTNAME'),
+            host=getenv('DB_HOST'),
             user=getenv('DB_USERNAME'),
             password=getenv('DB_PASSWORD')
         )
         cur = conn.cursor()
     except:
-        print("Error: Could not connect to the server.")
+        print("Error: Could not connect to the database server.")
         exit(1)
     return conn, cur
 
-def connect_to_database():
+def connect_to_database(db_name):
     try:
         conn = connect(
-            host=getenv('DB_HOSTNAME'),
+            host=getenv('DB_HOST'),
             user=getenv('DB_USERNAME'),
             password=getenv('DB_PASSWORD'),
-            dbname='sentiscore'
+            dbname=db_name
         )
         cur = conn.cursor()
     except OperationalError:
-        print("Error: Could not connect to the 'sentiscore' database.")
+        print(f"Error: Could not connect to the '{db_name}' database.")
         exit(1)
     return conn, cur
 
-def check_database_exists():
+def check_database_exists(db_name):
     conn, cur = connect_to_server()
     try:
         cur.execute("""
             SELECT EXISTS (
-                SELECT datname FROM pg_database
-                WHERE datname = 'sentiscore'
-            );
-        """)
+                SELECT datname FROM pg_database WHERE datname = %s
+            )
+        """, (db_name,))
         database_exists = cur.fetchone()[0]
     except OperationalError:
         print("Error: Could not check if database exists.")
@@ -50,18 +46,36 @@ def check_database_exists():
         conn.close()
     return database_exists
 
-def create_database():
-    conn, cur = connect_to_database()
-    conn.set_session(autocommit=True)
+def check_table_exists(db_name):
+    conn, cur = connect_to_database(db_name)
     try:
-        cur.execute('CREATE DATABASE sentiscore;')
+        cur.execute("""
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables
+                WHERE table_schema = 'public'
+                AND table_name = 'posts'
+            );
+        """)
+        table_exists = cur.fetchone()[0]
     except OperationalError:
-        print("Error: Could not create 'sentiscore' database.")
+        print("Error: Could not check if table exists.")
+        exit(1)
+    finally:
+        conn.close()
+    return table_exists
+
+def create_database(db_name):
+    conn, cur = connect_to_server()
+    conn.autocommit = True
+    try:
+        cur.execute(f"CREATE DATABASE {db_name};")
+    except OperationalError:
+        print(f"Error: Could not create '{db_name}' database.")
         exit(1)
 
-def create_table():
-    conn, cur = connect_to_database()
-    cur.execute('CREATE TABLE sentiscore (id serial PRIMARY KEY,'
+def create_table(db_name):
+    conn, cur = connect_to_database(db_name)
+    cur.execute('CREATE TABLE posts (id serial PRIMARY KEY,'
                                 'post_id VARCHAR(255) UNIQUE,'
                                 'post_text TEXT,'
                                 'created_at TIMESTAMP,'
--- projects/sentiscore/server.py
+++ projects/sentiscore/server.py
@@ -1,6 +1,10 @@
 # server.py
+from os import getenv
+from dotenv import load_dotenv
 from flask import Flask, render_template
-from database import check_database_exists, create_database
+from database import check_database_exists, create_database, check_table_exists, create_table
+
+load_dotenv(override=True)
 
 app = Flask(__name__)
 
@@ -9,13 +13,18 @@ def index():
     return render_template('index.html')
 
 def initialize_server():
-    if not check_database_exists():
-        print("Database 'sentiscore' does not exist.")
-        print("Creating 'sentiscore' database.")
-        create_database()
-        exit(1)
+    db_name = getenv('DB_NAME')
+    if not check_database_exists(db_name):
+        print(f"Database '{db_name}' does not exist.")
+        print(f"Creating '{db_name}' database.")
+        create_database(db_name)
+    elif not check_table_exists(db_name):
+        print("'posts' table does not exists.")
+        print("Creating 'sentiscore' table.")
+        create_table(db_name)
     else:
-        app.run()
+        print('The app started sucker.')
+        # app.run()
 
 if __name__ == '__main__':
     initialize_server()
