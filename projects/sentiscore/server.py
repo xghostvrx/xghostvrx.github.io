b47f24f7a599e5cb5f58b3d8e284b6a4c960ef5d
George Testo <xghostvrx@gmail.com>
2024-05-01 17:37:05 -0700

    Add logging module to server.py (creates server.log files)

--- projects/sentiscore/server.py
+++ projects/sentiscore/server.py
@@ -1,8 +1,12 @@
 # server.py
+import logging
 from os import getenv
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from database import check_columns, check_database_exists, create_database, check_table_exists, create_table, drop_table
+
+# Configure logging
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 load_dotenv(override=True)
 
@@ -15,20 +19,20 @@ def index():
 def initialize_server():
     db_name = getenv('DB_NAME')
     if not check_database_exists(db_name):
-        print(f"Database '{db_name}' does not exist.")
-        print(f"Creating '{db_name}' database.")
+        logging.info(f"Database '{db_name}' does not exist.")
+        logging.info(f"Creating '{db_name}' database.")
         create_database(db_name)
     elif not check_table_exists(db_name):
-        print("'posts' table does not exists.")
-        print("Creating 'posts' table.")
+        logging.info("'posts' table does not exists.")
+        logging.info("Creating 'posts' table.")
         create_table(db_name)
     elif not check_columns(db_name):
-        print("Data columns in 'posts' table do not appear to be appropriate.")
-        print("Dropping the 'posts' table and creating a new one.")
+        logging.warning("Data columns in 'posts' table do not appear to be appropriate.")
+        logging.warning("Dropping the 'posts' table and creating a new one.")
         drop_table(db_name)
         create_table(db_name)
     else:
-        print('The app started sucker.')
+        logging.info('The app started sucker.')
         # app.run()
 
 if __name__ == '__main__':
