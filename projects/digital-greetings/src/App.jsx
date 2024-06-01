0e63cff5ddd6a480e06789b8ccccd24ee641361d
George Testo <xghostvrx@gmail.com>
2024-06-01 13:46:03 -0700

    Add surprise animation effects (upon button press)

--- projects/digital-greetings/src/App.jsx
+++ projects/digital-greetings/src/App.jsx
@@ -1,19 +1,39 @@
-import landing from './assets/tejiendo-gatito.gif'
-import surprise from './assets/surprise.gif'
+import React, { useState } from 'react';
+import Confetti from 'react-confetti';
+import landing from './assets/tejiendo-gatito.gif';
+import surprise from './assets/surprise.gif';
 
 function App() {
+  const [showSurprise, setShowSurprise] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowSurprise(true);
+  };
 
   return (
     <>
       <div className='flex justify-center items-center h-screen'>
         <div className='text-center'>
-          <img src={landing} alt="Tejiendo Gatito" className='mx-auto mb-4'></img>
-          <img src={surprise} alt="Surprise" className='mx-auto mb-4'></img>
-          <button className='bg-slate-100 hover:bg-pink-500 hover:text-white py-2 px-4 rounded-lg button-effect'>Press for a Surprise</button>
+          <h1 className='text-5xl'>Guess what?!</h1>
+          {showSurprise && <Confetti />}
+          {showSurprise && <h1 className='text-5xl font-bold mb-4'>Happy Birthday</h1>}
+          <img
+            src={showSurprise ? surprise : landing}
+            alt={showSurprise ? 'Surprise' : 'Tejiendo Gatito'}
+            className='mx-auto mb-8'
+          />
+          {!showSurprise && (
+            <button
+              onClick={handleButtonClick}
+              className='bg-slate-100 hover:bg-pink-500 hover:text-white py-2 px-4 rounded-lg button-effect'
+            >
+              Press for a Surprise
+            </button>
+          )}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
