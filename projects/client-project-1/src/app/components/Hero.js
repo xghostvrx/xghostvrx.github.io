98261c5c81cc4121c543cc673d3b33600be61bed
George Testo <xghostvrx@gmail.com>
2024-06-02 16:36:39 -0700

    Add hero background styling

--- projects/client-project-1/src/app/components/Hero.js
+++ projects/client-project-1/src/app/components/Hero.js
@@ -3,9 +3,9 @@ import Header from "./Header";
 const Hero = () => {
     return (
         <div>
-            <section className="bg-cover bg-center h-screen font-sans text-center text-white">
+            <section className="film-grain bg-cover bg-center h-screen font-sans text-center text-white" style={{ backgroundImage: "url('/background.jpg')" }}>
                 <Header />
-                <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-50">
+                <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-30">
                     <div className="flex flex-col items-center justify-center h-full m-4 p-4">
                         <h1 className="text-4xl md:text-6xl font-bold">Unveiling the Sacred Tapestry&nbsp;of&nbsp;Life</h1>
                         <p className="m-2 p-1 text-white">Discover the healing power of Reiki and embark on a journey of spiritual&nbsp;growth&nbsp;and&nbsp;self-discovery.</p>
