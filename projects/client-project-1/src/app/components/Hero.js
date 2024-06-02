7362eb168adcae0b11f15be6245f68833c9d0fe6
George Testo <xghostvrx@gmail.com>
2024-06-02 13:36:31 -0700

    Add non-breaking spaces to hero text

--- projects/client-project-1/src/app/components/Hero.js
+++ projects/client-project-1/src/app/components/Hero.js
@@ -6,9 +6,9 @@ const Hero = () => {
             <section className="bg-cover bg-center h-screen font-sans text-center text-white">
                 <Header />
                 <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-50">
-                    <div className="flex flex-col items-center justify-center h-full m-4 sm:m-8 md:m-12 lg:m-16 xl:m-20 p-4 sm:p-8 md:p-12 lg:p-16 xl:p-20">
-                        <h1 className="text-4xl md:text-6xl font-bold">Unveiling the Sacred Tapestry of Life</h1>
-                        <p className="m-2 p-1 text-white">Discover the healing power of Reiki and embark on a journey of spiritual growth and self-discovery</p>
+                    <div className="flex flex-col items-center justify-center h-full m-4 p-4">
+                        <h1 className="text-4xl md:text-6xl font-bold">Unveiling the Sacred Tapestry&nbsp;of&nbsp;Life</h1>
+                        <p className="m-2 p-1 text-white">Discover the healing power of Reiki and embark on a journey of spiritual&nbsp;growth&nbsp;and&nbsp;self-discovery.</p>
                         <button className="mt-4 px-6 py-2 bg-white text-black rounded-lg hover:bg-black hover:text-white transition duration-300">Book a Session</button>
                     </div>
                 </div>
