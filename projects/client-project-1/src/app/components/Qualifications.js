6d8c1b3617e541379a95ff2ab9a67a24c95a5087
George Testo <xghostvrx@gmail.com>
2024-06-02 18:55:12 -0700

    Add box model adjustments

--- projects/client-project-1/src/app/components/Qualifications.js
+++ projects/client-project-1/src/app/components/Qualifications.js
@@ -1,23 +1,26 @@
-
 const Qualifications = () => (
-    <section id="qualifications" className="p-8 bg-gray-200 text-center">
-        <h2 className="text-3xl font-bold">Meet Your Spiritual Counselor</h2>
-        <div>
+    <section id="qualifications" className="p-8 bg-gray-100 text-center">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold">Meet Your Spiritual Counselor</h2>
+        <div className="my-4">
             <div className="bg-white rounded-lg">
                 <img src="https://placehold.co/250x250" alt="Image of Doris Bennett" className="w-64 h-64 object-cover rounded-lg mx-auto" />
             </div>
-            <h3 className="font-bold">Doris Bennett</h3>
-            <p>I know how it feels when the water stops running and you do not know where to turn. I have encountered many obstacles in my life. Being able to learn, adapt, and embrace my spirituality has allowed me to overcome and find purpose and fulfillment in my life.</p>
+            <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-4">Doris Bennett</h3>
+            <p className="mt-4 text-lg md:text-xl lg:text-2xl">
+                I know how it feels when the water stops running and you do not know where to turn. I have encountered many obstacles in my life. Being able to learn, adapt, and embrace my spirituality has allowed me to overcome and find purpose and fulfillment in my life.
+            </p>
         </div>
-        <div>
-            <h3>Certifications</h3>
-            <ul>
+        <div className="my-4">
+            <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold">Certifications</h3>
+            <ul className="mt-4 text-lg md:text-xl lg:text-2xl">
                 <li>Certified Chaplain (International Fellowship of Chaplains)</li>
                 <li>Advanced Reiki Training (Usui Shiki Ryoho Second Degree)</li>
                 <li>Certified Peer Support Specialist & Peer Recovery Mentor</li>
             </ul>
         </div>
-        <p>Through meditation, energy work, and ancient wisdom teachings, I assist you in cultivating a deep connection with your inner truth, allowing you to tap into the vast reservoir of wisdom and clarity that lies within. Whether you seek healing, personal growth, or a deeper sense of purpose, I will hold space for you to embrace your sacred essence and awaken to the boundless possibilities of your spiritual journey.</p>
+        <p className="mt-4 text-lg md:text-xl lg:text-2xl">
+            Through meditation, energy work, and ancient wisdom teachings, I assist you in cultivating a deep connection with your inner truth, allowing you to tap into the vast reservoir of wisdom and clarity that lies within. Whether you seek healing, personal growth, or a deeper sense of purpose, I will hold space for you to embrace your sacred essence and awaken to the boundless possibilities of your spiritual journey.
+        </p>
     </section>
 );
 
