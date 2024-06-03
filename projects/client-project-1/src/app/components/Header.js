3dfcca5184f500d18ef6fa1eff3503f3299240b4
George Testo <xghostvrx@gmail.com>
2024-06-03 09:59:12 -0700

    Add hamburger menu animation

--- projects/client-project-1/src/app/components/Header.js
+++ projects/client-project-1/src/app/components/Header.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -10,7 +11,7 @@ const Header = () => {
     }
 
     return (
-        <header className="absolute w-full bg-transparent font-sans text-lg md:text-2xl text-white p-4 md:p-8">
+        <header className="absolute w-full bg-transparent font-sans text-2xl text-white p-4 md:p-8">
             <nav className="flex justify-between">
                 <Link href="/" className='hover:text-black transition duration-300'>Sacred Spirituality</Link>
                 <div className="hidden md:flex space-x-10">
@@ -19,10 +20,12 @@ const Header = () => {
                     <Link href="#testimonials" className='hover:text-black transition duration-300'>Testimonials</Link>
                 </div>
                 <div className="md:hidden">
-                    <button onClick={toggleMenu} className='focus:outline-none'>
-                        <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
-                        </svg>
+                    <button onClick={toggleMenu}>
+                        <div className={classNames(`tham tham-e-squeeze tham-w-6`, { 'tham-active': menuOpen })}>
+                            <div className="tham-box">
+                                <div className="tham-inner bg-white" />
+                            </div>
+                        </div>
                     </button>
                 </div>
             </nav>
